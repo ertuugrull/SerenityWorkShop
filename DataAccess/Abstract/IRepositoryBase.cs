56ccca6b73eb83b613b7bb0f0427863ef3e8900e
Ertuğrul Şengül <ertuugrullsengul@hotmail.com>
2021-06-05 00:09:18 +0300

    Add DataAccessLayer

---
+++ DataAccess/Abstract/IRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    interface IRepositoryBase<T>
+    {
+        List<T> GetList(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T Entity);
+        void Delete(T Entity);
+        void Update(T Entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,66 @@
+﻿using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IRepositoryBase<TEntity>
+        where TEntity : class, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity Entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(Entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+
+            }
+        }
+
+        public void Delete(TEntity Entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(Entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null ?
+                context.Set<TEntity>().ToList() :
+                context.Set<TEntity>().Where(filter).ToList();
+
+            }
+
+        }
+
+        public void Update(TEntity Entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(Entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
